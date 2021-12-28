41d0079218b11a92a57748e7f390ae6d76f24f3c
Barbara Müller <xbarbaramuller@gmail.com>
2021-12-27 21:46:59 -0300

    Segundo commit: todo el resto de los archivos

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.querySelector(".menu__btn").addEventListener("click", () => {
+    document.querySelector(".nav__menu").classList.toggle("show");
+});
+  
+  ScrollReveal().reveal('.hero');
+  ScrollReveal().reveal('.card__server', { delay: 500 });
+  ScrollReveal().reveal('.banner__com', { delay: 500 });
