157097fa2d644c73e975b84f9aa22a9280514be4
Barbara Müller <xbarbaramuller@gmail.com>
2022-01-06 19:49:29 -0300

    Version final boot sin Axie

--- js/main.js
+++
@@ -1,7 +0,0 @@
-document.querySelector(".menu__btn").addEventListener("click", () => {
-    document.querySelector(".nav__menu").classList.toggle("show");
-});
-  
-  ScrollReveal().reveal('.hero');
-  ScrollReveal().reveal('.card__server', { delay: 500 });
-  ScrollReveal().reveal('.banner__com', { delay: 500 });
